511fc895a6bcb96206312caafc1d5770a451dabd
Teo Milicevic <tmilicevic@triadretail.com>
2020-09-23 16:35:17 -0400

    email from testing

--- main.py
+++ main.py
@@ -212,43 +212,43 @@ def scrape():
 		smtp_server = 'smtp.frontier.com'
 		subject = 'An email with attachment from Python'
 		body = 'This is an email with attachment sent from Python'
-		sender_email = 'cihusss@frontier.com'
+		sender_email = 'scraperdeamon@shopboxcommerce.com'
 		receiver_email = 'cihusss@gmail.com'
 		password = input('Type your password and press enter:')
 
-		# Create a multipart message and set headers
+		# create a multipart message and set headers
 		message = MIMEMultipart()
 		message['From'] = sender_email
 		message['To'] = receiver_email
 		message['Subject'] = subject
-		message['Bcc'] = receiver_email  # Recommended for mass emails
+		message['Bcc'] = receiver_email # recommended for mass emails
 
-		# Add body to email
+		# add body to email
 		message.attach(MIMEText(body, 'plain'))
 
-		filename = f'res_data_{fileStamp}.json'  # In same directory as script
+		filename = f'res_data_{fileStamp}.json' # in same directory as script
 
-		# Open PDF file in binary mode
+		# open a file in binary mode
 		with open(filename, 'rb') as attachment:
-		    # Add file as application/octet-stream
-		    # Email client can usually download this automatically as attachment
+		    # add file as application/octet-stream
+		    # email client can usually download this automatically as attachment
 		    part = MIMEBase('application', 'octet-stream')
 		    part.set_payload(attachment.read())
 
-		# Encode file in ASCII characters to send by email    
+		# encode file in ASCII characters to send by email    
 		encoders.encode_base64(part)
 
-		# Add header as key/value pair to attachment part
+		# add header as key/value pair to attachment part
 		part.add_header(
 		    'Content-Disposition',
 		    f'attachment; filename= {filename}',
 		)
 
-		# Add attachment to message and convert message to string
+		# add attachment to message and convert message to string
 		message.attach(part)
 		text = message.as_string()
 
-		# Log in to server using secure context and send email
+		# log in to server using secure context and send email
 		context = ssl.create_default_context()
 		with smtplib.SMTP_SSL(smtp_server, 465, context=context) as server:
 		    server.login(sender_email, password)
